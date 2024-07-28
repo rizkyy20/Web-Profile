6740601a29d1f0d8f403f69e8148ac9aeb878393
rizkyy20 <muhammadrizkyyamin@gmail.com>
2024-07-28 20:29:22 +0800

    Using AOS Library

---
+++ script.js
@@ -0,0 +1,19 @@
+function redirectIg() {
+    window.open("https://instagram.com/rzkyyy.20")
+}
+
+function redirectGit() {
+    window.open("https://github.com/rizkyy20")
+}
+
+function redirectLink() {
+    window.open("https://linkedin.com/in/Muhammad-Rizky-Yamin")
+}
+
+function redirectTiktok() {
+    window.open("https://tiktok.com/@froz208")
+}
+
+function email() {
+    window.location.href = "mailto:muhammadrizkyyamin@gmail.com"
+}
